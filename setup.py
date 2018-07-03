292230466769e4557ad7eec775a7981e8c3c067f
Josh Izaac <josh146@gmail.com>
2018-07-03 16:32:47 -0400

    added python version to qmlt setup.py

--- setup.py
+++ setup.py
@@ -65,6 +65,10 @@ classifiers = [
     "Operating System :: POSIX :: Linux",
     "Operating System :: Microsoft :: Windows",
     "Programming Language :: Python",
+    'Programming Language :: Python :: 3',
+    'Programming Language :: Python :: 3.5',
+    'Programming Language :: Python :: 3.6',
+    'Programming Language :: Python :: 3 :: Only',
     "Topic :: Scientific/Engineering :: Physics"
 ]
 
