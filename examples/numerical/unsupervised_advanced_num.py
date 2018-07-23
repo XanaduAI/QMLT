Small refactor of examples - unufied numerical and tf + small fixes.

examples/numerical/unsupervised_advanced_num.py
@@ -38,6 +38,7 @@ from qmlt.helpers import sample_from_distr
 
 # Number of layers
 depth = 5
+steps = 500
 
 # This time we use a dynamic way to create parameters for each layer
 my_params = []
@@ -74,9 +75,6 @@ def circuit(params):
     state = eng.run('fock', cutoff_dim=7)
     circuit_output = state.all_fock_probs()
 
-    #TODO: delete
-    circuit_output = np.reshape(circuit_output, [7, 7])
-
     return circuit_output
 
 
@@ -109,7 +107,7 @@ hyperparams = {'circuit': circuit,
 
 learner = CircuitLearner(hyperparams=hyperparams)
 
-learner.train_circuit(X=X_train, steps=500)
+learner.train_circuit(X=X_train, steps=steps)
 
 outcomes = learner.run_circuit()
 final_distribution = outcomes['outputs']